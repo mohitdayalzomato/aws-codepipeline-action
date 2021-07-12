712f46fc3a812c084c31ce11d345a7b424be78a2
mohitdayalzomato <mohit.dayal@zomato.com>
2021-07-12 15:23:53 +0530

    Removing credentials

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ var AWS = require("aws-sdk");
 var core = require("@actions/core");
 
 try {
-  var awsRegion = core.getInput("aws-region");
-  var awsAccessKey = core.getInput("aws-access-key");
-  var awssecretKey = core.getInput("aws-secret-key");
-  var pipelineName = core.getInput("pipeline-name");
-  var awsSessionToken = core.getInput("aws-session-token");
-
-  AWS.config = new AWS.Config();
-  AWS.config.region = awsRegion;
-  AWS.config.accessKeyId = awsAccessKey;
-  AWS.config.secretAccessKey = awssecretKey;
-  AWS.config.sessionToken = awsSessionToken;
+#  var awsRegion = core.getInput("aws-region");
+#  var awsAccessKey = core.getInput("aws-access-key");
+#  var awssecretKey = core.getInput("aws-secret-key");
+#  var pipelineName = core.getInput("pipeline-name");
+#  var awsSessionToken = core.getInput("aws-session-token");
+#
+#  AWS.config = new AWS.Config();
+#  AWS.config.region = awsRegion;
+#  AWS.config.accessKeyId = awsAccessKey;
+#  AWS.config.secretAccessKey = awssecretKey;
+#  AWS.config.sessionToken = awsSessionToken;
 
   var codepipeline = new AWS.CodePipeline();
   var pipeline = {
